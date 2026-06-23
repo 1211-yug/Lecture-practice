2e612f5523ea08b687c4ac671af81b6853fde582
yug patel <pyug31196@gmail.com>
2026-06-23 19:48:44 +0530

    Add DOM manipulation and button click functionality

--- Javascript/23-6-2026/script.js
+++ Javascript/23-6-2026/script.js
@@ -1,1 +1,24 @@
-console.log("Hello Javascript");
+console.log("Hello World");
+
+// Dom Element
+
+document.getElementById("firstHeading").innerHTML = "Hello Javascript"
+
+document.getElementById("paraOne").innerHTML = "This is JavaScripts"
+
+document.getElementById("paraOne").style.color = "red"
+
+const btn = document.getElementById("colorBtn");
+let isColorApplied = false;
+
+btn.addEventListener("click", () => {
+    if (isColorApplied === false) {
+        document.body.style.backgroundColor = "yellow";
+        isColorApplied = true;
+
+    } else {
+        document.body.style.backgroundColor = "";
+        isColorApplied = false;
+    }
+});
+
