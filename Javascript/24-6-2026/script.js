df9645539bcf8c4bfc8f583aabc4f01696b9e517
yug patel <pyug31196@gmail.com>
2026-06-24 12:07:00 +0530

    24-6-2026

---
+++ Javascript/24-6-2026/script.js
@@ -0,0 +1,25 @@
+// var Variables
+var fullName = "Yug Patel";
+
+console.log(fullName);
+
+// let Variables
+let profile = {
+    name: "Yug Patel",
+    age: 19,
+    city: "Surat"
+};
+
+console.log(profile);
+console.log(profile.name);
+console.log(profile.age);
+console.log(profile.city);
+
+// const Variables
+const hobbies = {
+    fullName: "Yug Patel",
+    age: 19,
+    city: "Surat"
+};
+
+console.log(hobbies);
