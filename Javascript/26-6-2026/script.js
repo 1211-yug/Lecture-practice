36d4aeeaa8b9880b3319e5a62db33e6494c0d8ed
yug patel <pyug31196@gmail.com>
2026-06-26 12:22:58 +0530

    26-6-2026

---
+++ Javascript/26-6-2026/script.js
@@ -0,0 +1,40 @@
+/* Javascript Built-in Constants */
+
+// null
+console.log("======= null ======");
+console.log(null);
+console.log(typeof null);
+
+// isNaN
+console.log("======= isNaN ======");
+console.log(isNaN(NaN));
+console.log(isNaN(100));
+
+// Number.isNaN()
+console.log("======= Number.isNaN() ======");
+console.log(Number.isNaN(NaN));
+console.log(Number.isNaN(100));
+
+// NaN
+console.log("======= NaN ======");
+console.log(NaN);
+
+// Unfinite
+console.log("======= Unfinite ======");
+
+let y;
+console.log(y);
+
+
+//Infinity
+console.log("======= Infinity & -Infinity ======");
+console.log(Infinity);
+console.log(-Infinity);
+
+// Circle of Area
+let radius = 3;
+let area = Math.PI * radius * radius;
+
+console.log("======= Circle of Area ======");
+
+console.log("Area =", area);
