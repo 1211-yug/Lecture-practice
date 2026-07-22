a038046eba63716a7f2caf4abd0965953a1072a4
yug patel <pyug31196@gmail.com>
2026-07-22 12:36:04 +0530

    22-7-2026

---
+++ Javascript/22-7-2026/script.js
@@ -0,0 +1,21 @@
+// javascript DOM
+
+// getElementbyid
+{
+    let dom = document.getElementById("firstHeading");
+    dom.style.textAlign = "center";
+    dom.style.color = "white";
+    dom.style.backgroundColor = "black";
+
+    console.log(dom);
+
+    console.log(dom.innerHTML);
+
+    console.log(dom.textContent);
+
+    console.log(dom.innerText)
+}
+
+{
+
+}
