63daaf06bd2bd5381fecf53e5b159790e5fb1bb3
yug patel <pyug31196@gmail.com>
2026-07-27 18:09:14 +0530

    Update script.js

--- Javascript/27-7-2026/script.js
+++ Javascript/27-7-2026/script.js
@@ -1,3 +1,4 @@
+// Sticky Header 
 const header = document.getElementById('myHeader');
 
 window.addEventListener('scroll', () => {
@@ -8,11 +9,46 @@ window.addEventListener('scroll', () => {
     }
 });
 
-
+// Background change on scroll
 window.addEventListener("scroll", () => {
     if (window.scrollY > 200) {
         document.body.style.background = "#1292c5ff"
     } else {
         document.body.style.background = "#4ba2c5ff"
     }
+})
+
+// Back To Top Btn
+const topBtn = document.getElementById("back-To-Top")
+const par = document.querySelectorAll("p");
+
+document.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+        par.forEach((p) => {
+            p.style.color = "black";
+        });
+        topBtn.style.display = "block"
+    } else {
+        par.forEach((p) => {
+            p.style.color = "white";
+        });
+        topBtn.style.display = "none"
+    }
+})
+
+topBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+})
+
+// Scroll Progress Bar
+const progressBar = document.getElementById('progressBar');
+
+window.addEventListener('scroll', () => {
+    const scrollableHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const scrolled = window.scrollY;
+    const percentage = (scrolled / scrollableHeight) * 100;
+    progressBar.style.width = percentage + '%';
 })
