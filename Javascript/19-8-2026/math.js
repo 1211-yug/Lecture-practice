4b72551b5b0d7b48582c2908998ebbfbc32b4680
yug patel <pyug31196@gmail.com>
2026-08-19 12:19:55 +0530

    19-8-2026 ( Export and Import )

---
+++ Javascript/19-8-2026/math.js
@@ -0,0 +1,41 @@
+// export function add(a, b) {
+//     return a + b;
+// }
+
+// export default class user {
+//     constructor(name) {
+//         this.name = name;
+//     }
+// }
+
+// export const random = Math.random();
+
+// function add1(a, b) {
+//     return a + b;
+// }
+// function mul(a, b) {
+//     return a * b;
+// }
+// module.exports = { add1, mul };
+
+/* ============ */
+// const config = {
+//     name: 'Yug',
+//     version: '1.0'
+// };
+
+// export default config;
+
+/* ============ */
+export default function add(a, b) {
+    return a + b;
+}
+
+export function sub(a, b) {
+    return a - b;
+}
+
+export function mul(a, b) {
+    return a * b;
+}
+
---
+++ Javascript/19-8-2026/script.js
@@ -0,0 +1,24 @@
+// import { add } from './math.js';
+
+// console.log(add(2, 3));
+
+// import { random } from './math.js';
+
+// console.log(random);
+
+/* ================= */
+// const { add1, mul } = require('./math.js');
+// console.log(add1(10, 20));
+// console.log(mul(10, 20));
+
+/* ================= */
+// import myConfig from './math.js';
+// console.log(myConfig.name);
+// console.log(myConfig.version);
+
+/* ================= */
+import add, { sub, mul } from './math.js';
+
+console.log(add(2, 3));
+console.log(sub(12, 2));
+console.log(mul(5, 5));
