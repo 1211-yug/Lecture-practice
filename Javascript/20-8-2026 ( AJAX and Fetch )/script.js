e42a8fbdeda9dd35b5bbc31b4f950757e09f3bdc
yug patel <pyug31196@gmail.com>
2026-08-20 23:14:09 +0530

    Refactor API functions for better structure

--- Javascript/20-8-2026 ( AJAX and Fetch )/script.js
+++ Javascript/20-8-2026 ( AJAX and Fetch )/script.js
@@ -4,6 +4,8 @@
 // Movie API
 // Cricket API
 
+/* --- Dog API --- */
+
 function getDog() {
     const url = "https://dog.ceo/api/breeds/image/random";
     const url1 = "https://dog.ceo/api/breeds/image/random";
@@ -56,26 +58,106 @@ function getDog() {
         })
 }
 
+/* --- Movies API --- */
+
 function getMovie() {
-    const url = "https://my-json-server.typicode.com/horizon-code-academy/fake-movies-api/movies";
+    const url = "http://localhost:3000/movies";
 
-    const random = url[Math.floor(Math.random() * url.length)]
+    const random = Math.floor(Math.random() * 12);
+    const random1 = Math.floor(Math.random() * 12);
+    const random2 = Math.floor(Math.random() * 12);
 
-    fetch(url, random)
+    fetch(url)
         .then(response => {
             return response.json();
         })
-        .then(random => {
-            console.log(random);
-            // document.getElementById("movieResult").innerHTML = `<div class="movieCard">
-            //     <img src="${random.Poster}" alt=""/>
-            //     <h3 class="mvtitle">${random.Title}</h3>
-            //     <h4 class="year">${random.Year}</h4>
-            //     <h5 class="time">${random.Runtime}</h5>
-            // </div>`
+        .then(movies => {
+            const movie = movies[random];
+            console.log(movie);
+            document.getElementById("movieResult").innerHTML = `<div class="movieCard">
+                    <img src="${movie.Poster}" alt="" />
+                    <h2 class="mvtitle">${movie.Title}</h2>
+                    <div class="flex yeartime">
+                        <h3 class="year">Year : ${movie.Year}</h3>
+                        <h3 class="time">Runtime : ${movie.Time}</h3>
+                    </div>
+                </div>`
+        })
+        .catch(error => {
+            console.log(error);
+            document.getElementById("movieResult").innerHTML = "Something went wrong"
+        })
+
+    fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(movies => {
+            const movie = movies[random1];
+            console.log(movie);
+            document.getElementById("movieResult1").innerHTML = `<div class="movieCard">
+                    <img src="${movie.Poster}" alt="" />
+                    <h2 class="mvtitle">${movie.Title}</h2>
+                    <div class="flex yeartime">
+                        <h3 class="year">Year : ${movie.Year}</h3>
+                        <h3 class="time">Runtime : ${movie.Time}</h3>
+                    </div>
+                </div>`
+        })
+        .catch(error => {
+            console.log(error);
+            document.getElementById("movieResult").innerHTML = "Something went wrong"
+        })
+
+    fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(movies => {
+            const movie = movies[random2];
+            console.log(movie);
+            document.getElementById("movieResult2").innerHTML = `<div class="movieCard">
+                    <img src="${movie.Poster}" alt="" />
+                    <h2 class="mvtitle">${movie.Title}</h2>
+                    <div class="flex yeartime">
+                        <h3 class="year">Year : ${movie.Year}</h3>
+                        <h3 class="time">Runtime : ${movie.Time}</h3>
+                    </div>
+                </div>`
         })
         .catch(error => {
             console.log(error);
             document.getElementById("movieResult").innerHTML = "Something went wrong"
         })
 }
+
+/* --- COVID-19 API --- */
+
+function getInfo() {
+
+    const val = document.querySelector("#country");
+    const country = String(val.value).toLowerCase();
+    const ctHd = document.getElementById("countryHeader");
+    const population = document.getElementById("population");
+    const totalCs = document.getElementById("totalCases");
+    const recv = document.getElementById("recovered");
+    const active = document.getElementById("activeCase");
+    const critical = document.getElementById("criticalCase");
+
+    fetch(`https://disease.sh/v3/covid-19/countries/${country}`)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            ctHd.innerText = `${data.country}`
+            population.innerText = `Total Population: ${data.population}`
+            totalCs.innerText = `Total Cases: ${data.cases}`
+            recv.innerText = `Recovered Cases: ${data.recovered}`
+            active.innerText = `Active Cases: ${data.active}`
+            critical.innerText = `Crititcal Cases: ${data.critical}`
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
